98b351ce89efadef12ff541dc9826abac8beab1f
Kemia Mbongo <kemiambongo@Kemias-MacBook-Pro.local>
2022-09-07 22:36:00 +0100

    travis file added

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=kemiamb
 export POSTGRES_PASSWORD=bombacita
-export POSTGRES_HOST=kemia-db.cofpoyik6cjn.us-east-1.rds.amazonaws.com
+export POSTGRES_HOST=kemiadb.ctgnionqafeb.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=kemiadb
-export AWS_BUCKET=kemia-bucket
-export AWS_REGION=us-east-1
+export AWS_BUCKET=kim-bucket-dev
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=hello
 export URL=http://localhost:8100
